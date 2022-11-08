cb35be9b7d657fbc262d4a0bee1a3b965e618389
martinskyler00 <112213373+martinskyler00@users.noreply.github.com>
2022-11-08 17:32:38 -0600

    fixed formatting

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 function init() {
-    //add your javascrip between these two lines of code
-        function makealert() {
-                var input = document.getElementById('entryinput').value;
-                var output = document.getElementById('textoutput');
-                output.innerHTML = input;
-                alert('Skyler Martin: ' + input);
-        }
-        
-        var vbutton = document.getElementById('entrybutton');
-                vbutton.addEventListener('click', makealert);
-    }
-    
-    
-    window.addEventListener('load', init);
+  //add your javascrip between these two lines of code
+  function makealert() {
+    var input = document.getElementById('entryinput').value;
+    var output = document.getElementById('textoutput');
+    output.innerHTML = input;
+    alert('Skyler Martin: ' + input);
+  }
+
+  var vbutton = document.getElementById('entrybutton');
+  vbutton.addEventListener('click', makealert);
+}
+
+
+window.addEventListener('load', init);
