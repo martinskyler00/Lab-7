15b256a8972fda6d18a797c84ff057ba3d87b0d1
martinskyler00 <112213373+martinskyler00@users.noreply.github.com>
2022-11-08 17:31:27 -0600

    fixed js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-function init(){
-function IOFunction(){
-            var input= document.getElementById('entryinput').value;
-            var output= document.getElementById('textoutput');
-            output.innerHTML= input;
-            alert("Skyler Martin: " + input);
+function init() {
+    //add your javascrip between these two lines of code
+        function makealert() {
+                var input = document.getElementById('entryinput').value;
+                var output = document.getElementById('textoutput');
+                output.innerHTML = input;
+                alert('Skyler Martin: ' + input);
         }
-        var vbutton= document.getElementById('entrybutton');
-            vbutton.addEventListener('click', makealert);
-}
-window.addEventListener('load', init);
+        
+        var vbutton = document.getElementById('entrybutton');
+                vbutton.addEventListener('click', makealert);
+    }
+    
+    
+    window.addEventListener('load', init);
